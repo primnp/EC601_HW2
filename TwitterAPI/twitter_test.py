7594f772d9c0e57d515d785761a1cd4d8b2826f7
Prim P <p.nuwapa@gmail.com>
2021-10-25 17:50:56 -0400

    Update twitter_test.py

--- TwitterAPI/twitter_test.py
+++ TwitterAPI/twitter_test.py
@@ -2,14 +2,28 @@ import tweepy
 import pandas as pd
 
 #twitter API credentials
-consumer_key = "your twitter API key"
-consumer_secret = "your twitter API secret"
-access_key = "your twitter access key"
-access_secret = "your twitter access secret"
+# consumer_key = "your twitter API key"
+# consumer_secret = "your twitter API secret"
+# access_key = "your twitter access key"
+# access_secret = "your twitter access secret"
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth)
+def tweepy_api():
+    #get secrets from github
+    cons_key = os.getenv("CONS_KEY")
+    cons_secret = os.getenv("CONS_SECRET")
+    accs_key = os.getenv("ACCESS_KEY")
+    accs_secret = os.getenv("ACCESS_SECRET")
+
+    #twitter api credentials set up
+    auth = tweepy.OAuthHandler(cons_key, cons_secret)
+    auth.set_access_token(accs_key, accs_secret)
+    api = tweepy.API(auth)
+    return api
+
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_key, access_secret)
+# api = tweepy.API(auth)
+api = tweepy_api()
 
 no_of_tweets = 46
 tweets = [] #full_text

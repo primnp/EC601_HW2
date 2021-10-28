0761191f0336e16c03a31f8651ef58c43a1f9ea9
primnp <51148602+primnp@users.noreply.github.com>
2021-10-28 18:27:29 -0400

    Update twitter_t.py

--- TwitterAPI/twitter_t.py
+++ TwitterAPI/twitter_t.py
@@ -2,28 +2,21 @@ import tweepy
 import pandas as pd
 import os
 
-#twitter API credentials
-# consumer_key = "your twitter API key"
-# consumer_secret = "your twitter API secret"
-# access_key = "your twitter access key"
-# access_secret = "your twitter access secret"
-
+# function to set up tweepy
 def tweepy_api():
-    #get secrets from github
+    # get secrets from github
     cons_key = os.getenv("CONS_KEY")
     cons_secret = os.getenv("CONS_SECRET")
     accs_key = os.getenv("ACCESS_KEY")
     accs_secret = os.getenv("ACCESS_SECRET")
 
-    #twitter api credentials set up
+    # twitter api credentials set up
     auth = tweepy.OAuthHandler(cons_key, cons_secret)
     auth.set_access_token(accs_key, accs_secret)
     api = tweepy.API(auth)
     return api
 
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_key, access_secret)
-# api = tweepy.API(auth)
+# call tweepy function to use the twitter api 
 api = tweepy_api()
 
 no_of_tweets = 46
@@ -32,7 +25,7 @@ likes = [] #favorite_count
 time = [] #created_at
 hashtag = [] #hashtag
 
-#retrieve 46 most recent tweets from a specific user (in this case, it's @danielricciardo)
+# retrieve 46 most recent tweets from a specific user (in this case, it's @danielricciardo)
 cursor = tweepy.Cursor(api.user_timeline, id="danielricciardo", tweet_mode="extended").items(no_of_tweets)
 
 for i in cursor:
@@ -42,6 +35,6 @@ for i in cursor:
     hash = (i.entities['hashtags'])
     hashtag.append(hash)
 
-#display retrieved information in dataframe format
+# display retrieved information in dataframe format
 df = pd.DataFrame({'tweets': tweets, 'likes': likes, 'time': time, 'hashtag': hashtag})
 print(df.to_string())
